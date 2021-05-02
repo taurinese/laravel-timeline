04317041b31fc54de60da0c8002835b1bba88625
Enzo Taurines <enzodylan.taurines@gmail.com>
2021-05-02 22:44:36 +0200

    Added seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,30 @@
 
 namespace Database\Seeders;
 
+use Carbon\Factory;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
+
+    
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => Hash::make('password'),
+            'image' => 'avatar.png'
+        ]);
+
+        $usersId = DB::table('users')->pluck('id')->toArray();
+        DB::table('posts')->insert([
+            'body' => Str::random(50),
+            'user_id' => Arr::random($usersId)
+        ]);
     }
 }
